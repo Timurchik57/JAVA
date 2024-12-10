9e7adeb62a99d6ecfc17a8e5dad537829f649a3b
Timur <timur_galiakberov@mail.ru>
2024-12-10 10:33:06 +0500

    Изменения  10.12.2024

--- src/test/java/UI/JAVATest.java
+++ src/test/java/UI/JAVATest.java
@@ -34,6 +34,12 @@ public class JAVATest extends Abstract {
 
     @Test
     @DisplayName("Тест для проверки Cherry-Pick")
+    public void ReadFile() throws IOException {
+        ReadProp("src/test/resources/my.properties", "Token_TmTest");
+    }
+
+    @Test
+    @DisplayName("Тест для проверки Cherry-Pick")
     public void InputFile() throws IOException {
         InputProp("src/test/resources/my.properties", "Token_TmTest", "Token");
     }
